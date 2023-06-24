0a1ce80a544cf8a27ae689f0e024469989b4f0d9
nasuda <nasuda@example.com>
2023-06-24 13:51:25 +0900

    add index.html content and index.js code

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
-const res = fetch('./hello.json');
+//import { app, reqJson, rm } from './src/App.js';
+//import { hanakoJson, hanakoObj } from './src/json.js';
+//import check
+
+
+function reqJson() {
+  const res = fetch('./jsonfiles/hello.json');
+  console.log(res);
+
+  const dataObj = res.then((res) => {
+    return res.json();
+  });
+  console.log(dataObj);
+  const objMessage = dataObj.then((dataObjstyle="display: block;") => {
+    return dataObj.message;
+  }) 
+  console.log(objMessage);
+
+  const messageElm = document.getElementById('message');
+  dataObj.then((obj) => {
+    messageElm.innerHTML = obj.message;
+  });  
+}
+
+const res = fetch('./jsonfiles/hello.json');
 console.log(res);
 
-const data = res.then((res) => {
+const dataObj = res.then((res) => {
   return res.json();
 });
+console.log(dataObj);
+const objMessage = dataObj.then((dataObj) => {
+  return dataObj.message;
+}) 
+console.log(objMessage);
 
-console.log(data);
+const messageElm = document.getElementById('message');
+dataObj.then((obj) => {
+  messageElm.innerHTML = obj.message;
+});
+
+function rm() {
+  messageElm.innerHTML = '';
+}
+
+/*
+const dataJson = dataObj.then((Obj) => {
+  return JSON.stringify(Obj);
+});
+console.log(dataJson);
+*/
+
+
+/*
+const resHanako = fetch('./jsonfiles/hanako.json');
+console.log(resHanako);
+
+const dataHanako = resHanako.then((resHanako) => {
+  return resHanako.json();
+});
+console.log(dataHanako);
+*/
+
+//log(hanakoObj);
+//log(hanakoJson);
